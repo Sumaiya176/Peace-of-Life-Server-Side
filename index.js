4ad9666ee606307379dae8c52eccb198f16ca4e3
Sumaiya176 <sumaiya.tr96@gmail.com>
2021-04-18 20:02:58 +0600

    code

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ijdq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
